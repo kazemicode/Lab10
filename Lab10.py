4421c0f2ae4c37213a564cc4dafcbe8771fafbd7
kazemicode <sarieses@gmail.com>
2018-11-17 19:46:08 -0800

    Cleaned up with comments

--- Lab10.py
+++ Lab10.py
@@ -10,29 +10,30 @@ wordTuple = ("awkward", "banjo", "crypt", "dwarf", "fever", "fjord", "gaze", "gy
 def main():
     printInstructions()
     initializeGame()     
-     
+
+# Print the instructions in a pop-up window     
 def printInstructions():
   showInformation("Welcome to Hangman!\n\
 Try to guess which letters are in the secret word. \
 You have six guesses.")
 
+# Initialize and play the game until word is guessed or out of guesses
 def initializeGame():
-  secretWord = pickAWord(wordTuple)
-  copy = secretWord
-  gameBoard = getGameBoard(len(secretWord))
-  while guesses > 0 and copy != gameBoard:
+  secretWord = pickAWord(wordTuple)         # An ordered list of letters in word
+  gameBoard = getGameBoard(len(secretWord)) # Initally a list of spaces
+  while guesses > 0 and secretWord != gameBoard:
     print "You have ", guesses, " guesses remaining."
     print "Guess the word:"
     print
     print
     printGameBoard(gameBoard) 
     checkLetter(secretWord, gameBoard)
-  if guesses <= 0:
+  if guesses <= 0:                  # Lose condition
     print "Out of guesses! You lose!"
-  elif copy == gameBoard:
+  elif copy == gameBoard:           # Win condition
     printGameBoard(gameBoard) 
     print "You win!"
-  print "Play again!"
+  print "Play again!"               
 
    
 # Given a tuple of words, pick one at random
@@ -45,21 +46,28 @@ def pickAWord(tuple):
   for index in range(0, len(word)):
     letterList.append(word[index])
   return letterList
-  
+
+# Makes a list of empty spaces
+# Length of this list is as long as the secret word
 def getGameBoard(length):
   gameBoard = []
   for x in range(0, length):
     gameBoard.append("__")
   return gameBoard
        
-  
+# Prints out the current state of the game
+# Includes guessed letters and blanks where letters
+# are still unguessed 
 def printGameBoard(gameBoard):
   for element in gameBoard:
     print element ,
   print
   print
     
-    
+# Enumerates the ordered list of letters in the word 
+# for each position that matches the guessed letter,
+# replace the blank on the gameboard at that location
+# with the guessed letter
 def checkLetter(word, gameBoard):
   guess = requestString('Enter a letter').lower()
   # replace every letter in word that matches the guess
